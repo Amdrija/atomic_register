c84c6d513e838af2732d518f5379e8d9d1fd721e
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2024-10-23 17:39:49 +0200

    Refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,25 @@
+use std::process::exit;
 use crate::abd::ABD;
 use crate::config::Config;
 use crate::network::initialize_connections;
+use anyhow::Result;
 use futures::future::TryJoinAll;
 use log::{error, info};
-use rkyv::{Archive, Deserialize, Serialize};
-use std::fmt::Debug;
 use std::sync::Arc;
-use std::time::{Duration, SystemTime};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use tokio::sync::mpsc;
-use anyhow::Result;
 
 mod abd;
 mod config;
 mod core;
 mod network;
 
-#[derive(Clone, Debug, Serialize, Deserialize, Archive)]
-struct Message {
-    text: String,
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    pretty_env_logger::init_timed();
-    info!("Started");
-    let parse_result = Config::new();
-    if let Err(error) = parse_result {
-        error!("Error when parsing config: {error}");
-        return Err(error);
-    }
-    let config = parse_result.unwrap();
+async fn run() -> Result<()> {info!("Started");
+    let config = Config::new()?;
     info!("Finished parsing config: {:#?}", config);
 
-    let result = initialize_connections(&config).await;
-    if let Err(error) = result {
-        error!("Failed to initialize connections: {error}");
-        return Err(error);
-    }
+    let (virtual_network, receiver_channel, read_tasks, send_loop_tasks) = initialize_connections(&config).await?;
     info!("Initialized connections to all nodes");
-    let (virtual_network, receiver_channel, read_tasks, send_loop_tasks) = result.unwrap();
 
     let reader_handles = read_tasks
         .into_iter()
@@ -56,53 +37,67 @@ async fn main() -> Result<()> {
         abd_clone.receive_loop(receiver_channel, quit_signal).await;
     });
 
-    let before_write = SystemTime::now();
-    abd.write(1, 10 + config.my_node_id as u32).await.unwrap();
-    let after_write = SystemTime::now();
-    println!(
-        "Node {} wrote key {}: {} time: {}ms",
-        config.my_node_id,
-        1,
-        10 + config.my_node_id as u32,
-        after_write
-            .duration_since(before_write)
-            .unwrap()
-            .as_millis()
+    // Wait for everyone to connect because of different message propagation times
+    // In theory, it shouldn't take more than max 1.5 * RTT time of all node connections
+    info!(
+        "Node {}: waiting 2s for everyone to connect",
+        config.my_node_id
     );
+    tokio::time::sleep(Duration::from_secs(2)).await;
+    println!(
+        "Node {}: experiment started at: {:.3}",
+        config.my_node_id,
+        SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs_f64()
+    );
+
+    if config.my_node_id == 2 {
+        let before_write = SystemTime::now();
+        abd.write(1, 10 + config.my_node_id as u32).await?;
+        let after_write = SystemTime::now();
+        println!(
+            "Node {} wrote key {}: {} time: {}ms",
+            config.my_node_id,
+            1,
+            10 + config.my_node_id as u32,
+            after_write
+                .duration_since(before_write)?
+                .as_millis()
+        );
+    }
 
     for (node, _) in config.nodes {
         let before_read = SystemTime::now();
-        let read = abd.read(node as u64).await.unwrap();
+        let read = abd.read(node as u64).await?;
         let after_read = SystemTime::now();
         println!(
             "Node {} read key {}: {} time: {}ms",
             config.my_node_id,
             node,
             read,
-            after_read.duration_since(before_read).unwrap().as_millis()
+            after_read.duration_since(before_read)?.as_millis()
         );
     }
 
     tokio::time::sleep(Duration::from_secs(5)).await;
     info!("Initiated exit");
-    quit.send(()).await.unwrap();
+    quit.send(()).await?;
 
     //Cleanup
     info!("Cleaning up and exiting");
-    receive_loop_handle.await.unwrap();
+    receive_loop_handle.await?;
     drop(abd);
 
-    let read_result = reader_handles.into_iter().collect::<TryJoinAll<_>>().await;
-    if let Err(error) = read_result {
-        error!("Error encountered while reading: {error}");
-        return Err(error.into());
-    }
-
-    let send_result = sender_handles.into_iter().collect::<TryJoinAll<_>>().await;
-    if let Err(error) = send_result {
-        error!("Error encountered while sending: {error}");
-        return Err(error.into());
-    }
+    reader_handles.into_iter().collect::<TryJoinAll<_>>().await?.into_iter().collect::<Result<_>>()?;
+    sender_handles.into_iter().collect::<TryJoinAll<_>>().await?.into_iter().collect::<Result<_>>()?;
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init_timed();
+    if let Err(error) = run().await {
+        error!("Error encountered while sending: {error}");
+        exit(1);
+    }
 }
