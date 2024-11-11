915918478bda62ce4b3860023d865cc7606b1402
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2024-11-11 15:27:41 +0100

    Refactor multipaxos state into separate object

--- src/multipaxos.rs
+++ src/multipaxos.rs
@@ -1,10 +1,14 @@
 use crate::core::{NodeId, Packet, VirtualNetwork};
+use anyhow::{anyhow, Result};
 use futures::future::JoinAll;
 use log::{debug, info};
 use rkyv::{Archive, Deserialize, Serialize};
 use std::cmp::min;
 use std::future::Future;
+use std::ops::Deref;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicU16, Ordering};
+use std::thread::sleep;
 use std::time::{Duration, Instant};
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::{mpsc, Mutex};
@@ -18,13 +22,13 @@ struct ProposalNumber {
     node: NodeId,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Archive)]
+#[derive(Debug, Clone, Serialize, Deserialize, Archive, PartialEq, Eq)]
 struct WriteCommand {
     key: u64,
     value: u32,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Archive)]
+#[derive(Debug, Clone, Serialize, Deserialize, Archive, PartialEq, Eq)]
 enum Command {
     NoOp,
     Write(WriteCommand),
@@ -38,8 +42,14 @@ struct PrepareMessage {
 
 #[derive(Debug, Clone, Serialize, Deserialize, Archive)]
 struct PromiseMessage {
+    proposal_number: ProposalNumber,
     accepted: Option<LogEntry>,
     no_more_accepted: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Archive)]
+struct RejectPrepareMessage {
+    proposal_number: ProposalNumber
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Archive)]
@@ -73,6 +83,7 @@ enum Message {
     Heartbeat,
     Prepare(PrepareMessage),
     Promise(PromiseMessage),
+    RejectPrepare(RejectPrepareMessage),
     Propose(ProposeMessage),
     Accept(AcceptMessage),
     Success(SuccessMessage),
@@ -85,56 +96,102 @@ struct LogEntry {
     command: Command,
 }
 
+const INFINITE_PROPOSAL: ProposalNumber = ProposalNumber {
+    round: u64::MAX,
+    node: NodeId::MAX,
+};
+
+struct MultipaxosState {
+    node: NodeId,
+    leader: NodeId,
+    heard_from: Vec<NodeId>,
+    log: Vec<Option<LogEntry>>,
+    min_proposal: ProposalNumber,
+    max_round: u64,
+    next_index: usize,
+    prepared: bool,
+}
+
+impl MultipaxosState {
+    fn new(node: NodeId) -> Self {
+        let zero_proposal = ProposalNumber {
+            round: 0,
+            node,
+        };
+
+        Self {
+            node: node,
+            leader: node,
+            heard_from: vec![],
+            log: vec![Some(LogEntry {
+                proposal_number: zero_proposal.clone(),
+                command: Command::NoOp,
+            })],
+            min_proposal: zero_proposal.clone(),
+            max_round: zero_proposal.round,
+            next_index: 1,
+            prepared: false
+        }
+    }
+
+    fn elect_leader(&mut self) {
+        debug!("Node {} heard from: {:?}", self.node, self.heard_from);
+        let highest_heard = self.heard_from.iter().max().unwrap_or(&self.node);
+        self.leader = *highest_heard;
+        debug!("Node {} elected {} for leader", self.node, self.leader);
+        self.heard_from.clear();
+    }
+
+    fn set_log(
+        &mut self,
+        index: usize,
+        proposal_number: ProposalNumber,
+        command: Command,
+    ) {
+        if index >= self.log.len() {
+            self.log.reserve(index - self.log.len() + 1);
+        }
+        self.log[index] = Some(LogEntry {
+            proposal_number,
+            command,
+        });
+    }
+
+    fn first_unchosen_index(&self) -> usize {
+        self.log.iter()
+            .enumerate()
+            .find(|(index, entry)| {
+                entry.is_some() && entry.as_ref().unwrap().proposal_number < INFINITE_PROPOSAL
+            })
+            .map(|(index, _)| index)
+            .unwrap_or(self.log.len())
+    }
+}
+
 struct Multipaxos {
     network: VirtualNetwork<Message>,
-    leader: Arc<Mutex<NodeId>>,
-    leader_channel: Sender<NodeId>,
     heartbeat_delay: Duration,
-    log: Arc<Mutex<Vec<LogEntry>>>,
-    last_log_index: Arc<Mutex<usize>>,
-    min_proposal: Arc<Mutex<ProposalNumber>>,
-    max_round: Arc<Mutex<u64>>,
-    next_index: Arc<Mutex<usize>>,
-    prepared: Arc<Mutex<bool>>,
-    cancellation_token: CancellationToken,
-    heard_from: Arc<Mutex<Vec<NodeId>>>,
+    state: Arc<Mutex<MultipaxosState>>,
+    cancellation_token: CancellationToken
 }
 
 impl Multipaxos {
-    fn new(
+    pub fn new(
         virtual_network: VirtualNetwork<Message>,
         heartbeat_delay: Duration,
         receive_channel: Receiver<Packet<Message>>,
     ) -> (Arc<Self>, impl Future) {
-        let min_proposal = ProposalNumber {
-            round: 0,
-            node: virtual_network.node,
-        };
-
-        let (leader_send, leader_receive) = mpsc::channel(20);
+        let node = virtual_network.node;
         let multipaxos = Arc::new(Multipaxos {
             network: virtual_network,
-            leader: Arc::new(Mutex::new(min_proposal.node)),
-            leader_channel: leader_send,
             heartbeat_delay,
-            log: Arc::new(Mutex::new(vec![LogEntry {
-                proposal_number: min_proposal.clone(),
-                command: Command::NoOp,
-            }])),
-            last_log_index: Arc::new(Mutex::new(0)),
-            min_proposal: Arc::new(Mutex::new(min_proposal)),
-            max_round: Arc::new(Mutex::new(0)),
-            next_index: Arc::new(Mutex::new(1)),
-            prepared: Arc::new(Mutex::new(false)),
-            cancellation_token: CancellationToken::new(),
-            heard_from: Arc::new(Mutex::new(Vec::new())),
+            state: Arc::new(Mutex::new(MultipaxosState::new(node))),
+            cancellation_token: CancellationToken::new()
         });
 
         let multipaxos_clone = multipaxos.clone();
         let heartbeat_procssing_handle = tokio::spawn(async move {
-            multipaxos_clone
-                .heartbeat_processing_loop(leader_receive)
-                .await;
+            multipaxos_clone.heartbeat_processing_loop().await;
         });
 
         let multipaxos_clone = multipaxos.clone();
@@ -156,6 +213,14 @@ impl Multipaxos {
         (multipaxos, cleanup_task)
     }
 
+    // No point in issuing multiple concurrent commands
+    // because the prepare phase will have to run for all
+    // and then they will potentially live-lock each other
+    pub async fn issue_command(&self, command: Command) -> Result<()> {
+        // TODO: Implement
+        Ok(())
+    }
+
     pub fn quit(&self) {
         self.cancellation_token.cancel();
     }
@@ -172,12 +237,13 @@ impl Multipaxos {
                     let packet = result.unwrap();
                     match packet.data {
                         Message::Heartbeat => {self.process_heartbeat(packet.from).await}
-                        Message::Prepare(_) => {}
-                        Message::Promise(_) => {}
-                        Message::Propose(_) => {}
-                        Message::Accept(_) => {}
-                        Message::Success(_) => {}
-                        Message::SuccessResponse(_) => {}
+                        Message::Prepare(prepare) => {self.process_prepare(prepare, packet.from).await}
+                        Message::RejectPrepare(reject_prepare) => {self.process_reject_prepare(reject_prepare).await}
+                        Message::Promise(promise) => {self.process_promise(promise).await}
+                        Message::Propose(propose) => {self.process_propose(propose, packet.from).await}
+                        Message::Accept(accept) => {self.process_accept(accept, packet.from).await}
+                        Message::Success(success) => {self.process_success(success, packet.from).await}
+                        Message::SuccessResponse(success_response) => {self.process_success_ressponse(success_response, packet.from).await}
                     };
                 }
                 _ = self.cancellation_token.cancelled() => {
@@ -204,18 +270,13 @@ impl Multipaxos {
         }
     }
 
-    async fn heartbeat_processing_loop(&self, mut leader_receiver: Receiver<NodeId>) {
+    async fn heartbeat_processing_loop(&self) {
         let mut interval = tokio::time::interval(self.heartbeat_delay * 2);
         loop {
             select! {
                 _ = interval.tick() => {
-                    let mut leader = self.leader.lock().await;
-                    let mut heard_from = self.heard_from.lock().await;
-                    debug!("Node {} heard from: {:#?}", self.network.node, *heard_from);
-                    let highest_heard = *heard_from.iter().max().unwrap_or(&self.network.node);
-                    *leader = highest_heard;
-                    debug!("Node {} elected {} for leader", self.network.node, *leader);
-                    heard_from.clear();
+                    let mut state = self.state.lock().await;
+                    state.elect_leader();
                 }
                 _ = self.cancellation_token.cancelled() => {
                     info!("Terminated heartbeat processing loop");
@@ -226,16 +287,49 @@ impl Multipaxos {
     }
 
     async fn process_heartbeat(&self, from: NodeId) {
+        //TODO: Implement
         debug!("Node {} received heartbeat from {from}", self.network.node);
-        let mut heard_from = self.heard_from.lock().await;
-        heard_from.push(from);
+        let mut state = self.state.lock().await;
+        state.heard_from.push(from);
+    }
+
+    async fn process_prepare(&self, prepare_message: PrepareMessage, from: NodeId) {
+        // TODO: Implement
+    }
+
+    async fn process_reject_prepare(&self, reject_prepare_message: RejectPrepareMessage) {
+        // TODO: Implement
+    }
+
+    async fn process_promise(&self, promise_message: PromiseMessage) {
+        // TODO: Implement
+    }
+
+    async fn process_propose(&self, propose_message: ProposeMessage, from: NodeId) {
+        // TODO: Implement
+    }
+
+    async fn process_accept(&self, accept_message: AcceptMessage, from: NodeId) {
+        // TODO: Implement
+    }
+
+    async fn process_success(&self, success_message: SuccessMessage, from: NodeId) {
+        // TODO: Implement
+    }
+
+    async fn process_success_ressponse(
+        &self,
+        success_response_message: SuccessResponseMessage,
+        from: NodeId,
+    ) {
+        // TODO: Implement
     }
 }
 
 #[cfg(test)]
 mod test {
     use crate::core::create_channel_network;
-    use crate::multipaxos::Multipaxos;
+    use crate::multipaxos::{Command, Multipaxos, WriteCommand};
     use futures::future::JoinAll;
     use log::{debug, log_enabled, Level};
     use std::collections::HashMap;
@@ -257,19 +351,19 @@ mod test {
             let network = virtual_networks.remove(node).unwrap();
             let receive_channel = receivers.remove(node).unwrap();
             let (multipaxos, background_tasks) =
-                Multipaxos::new(network, Duration::from_millis(10), receive_channel);
+                Multipaxos::new(network, Duration::from_millis(100), receive_channel);
             multipaxoses.insert(node, multipaxos);
             spawned_tasks.push(background_tasks);
         }
 
-        tokio::time::sleep(Duration::from_millis(32)).await;
+        tokio::time::sleep(Duration::from_millis(320)).await;
 
-        let mp4 = multipaxoses.remove(&4).unwrap();
-        mp4.quit();
-        drop(mp4);
-        debug!("***** KILLED 4 *****");
+        let result = multipaxoses[&0].issue_command(Command::Write(WriteCommand{
+            key: 1,
+            value: 10
+        })).await;
 
-        tokio::time::sleep(Duration::from_millis(66)).await;
+        assert!(result.is_ok());
 
         for node in &nodes {
             multipaxoses[node].quit();
